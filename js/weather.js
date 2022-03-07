3312733deb3022ac2b145dc55d9e5c5028026b51
JongHoonSon <roka1684@gmail.com>
2022-03-08 08:45:35 +0900

    Refactor weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,27 +1,33 @@
 const API_KEY = "8b0c0adedf66f705d3038e86b1712086";
 
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError)
+
 function onGeoSuccess(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log(lat, lon);
     const weatherAPIUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-    console.log(weatherAPIUrl);
-    fetch(weatherAPIUrl)
-    .then(response => response.json())
-    .then(data => {
-        const locationName = data.name;
-        const locationWeather = data.weather[0].main;
-        const locationTemp = data.main.temp;
-        const locationNameContainer = document.querySelector("#weather span:first-child");
-        const lcationWeatherContainer = document.querySelector("#weather span:last-child");
-
-        locationNameContainer.innerText = locationName;
-        lcationWeatherContainer.innerText = `${locationWeather} / ${locationTemp}°C`;
-    });
+    getWeatherData(weatherAPIUrl);
 }
 
 function onGeoError() {
     alert("Can't find you. No weather for you");
 }
 
-navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError)
+function getWeatherData(weatherAPIUrl) {
+    fetch(weatherAPIUrl)
+    .then(response => response.json())
+    .then(data => {
+        const locationName = data.name;
+        const locationWeather = data.weather[0].main;
+        const locationTemp = data.main.temp;
+        paintWeather(locationName, locationWeather, locationTemp);
+    });
+}
+
+function paintWeather(locationName, locationWeather, locationTemp) {
+    const locationNameContainer = document.querySelector("#weather span:first-child");
+    const lcationWeatherContainer = document.querySelector("#weather span:last-child");
+
+    locationNameContainer.innerText = locationName;
+    lcationWeatherContainer.innerText = `${locationWeather} / ${locationTemp}°C`;
+}
