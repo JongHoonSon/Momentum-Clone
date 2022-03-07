c6073bf91aab5b795a4ed870d98651c9a0e9ae92
JongHoonSon <roka1684@gmail.com>
2022-03-08 08:55:16 +0900

    Rename JS files

---
+++ js/login.js
@@ -0,0 +1,32 @@
+const $loginForm = document.querySelector("#login-form");
+const $loginInput = document.querySelector("#login-form input");
+const $greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+checkUserLogin();
+
+function checkUserLogin() {
+    const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+    if(savedUsername === null) {
+        $loginForm.classList.remove(HIDDEN_CLASSNAME);
+        $loginForm.addEventListener("submit", onLoginSubmit);
+    } else {
+        paintGreetings(savedUsername);
+    }
+}
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    $loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = $loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    $greeting.innerHTML = `Hello ${username}`;
+    $greeting.classList.remove(HIDDEN_CLASSNAME);
+}
